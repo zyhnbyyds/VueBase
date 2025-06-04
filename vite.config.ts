4a556885bbf380736ce168e0b258dd6be3e593c9
zyhnbyyds <19939926438@163.com>
2025-06-04 23:10:32 +0800

    build: time fly

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
-import Components from 'unplugin-vue-components/vite'
-import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
-import VueMacros from 'unplugin-vue-macros/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   resolve: {
@@ -14,23 +12,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    VueMacros({
-      defineOptions: false,
-      defineModels: false,
-      plugins: {
-        vue: Vue({
-          script: {
-            propsDestructure: true,
-            defineModel: true,
-          },
-        }),
-      },
-    }),
-
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages(),
-
-    // https://github.com/antfu/unplugin-auto-import
     AutoImport({
       imports: [
         'vue',
@@ -43,15 +24,11 @@ export default defineConfig({
       ],
       vueTemplate: true,
     }),
-
-    // https://github.com/antfu/vite-plugin-components
     Components({
       dts: true,
     }),
-
-    // https://github.com/antfu/unocss
-    // see uno.config.ts for config
     UnoCSS(),
+    Vue(),
   ],
 
   // https://github.com/vitest-dev/vitest
